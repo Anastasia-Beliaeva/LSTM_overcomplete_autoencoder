3d20bb384410488eddc880208dc78cca7150c15e
anastasiabelaeva <belyaeva.a.u@yandex.ru>
2024-08-22 17:09:35 +0300

    the autoencoder model upload

---
+++ The autoencoder.py
@@ -0,0 +1,288 @@
+import torch
+import copy
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from torch import nn
+import statistics
+import pathlib
+
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+
+base_path = pathlib.Path(__file__).parent
+temp_path = base_path.joinpath('temp_dir')
+normal_df = pd.read_excel(base_path.joinpath('lstm_regular_df.csv'))
+anomaly_df = pd.read_excel(base_path.joinpath('lstm_anomaly_df.csv'))
+
+
+train_df, val_df = train_test_split(
+  normal_df,
+  test_size=0.15,
+  random_state=RANDOM_SEED
+)
+
+val_df, test_df = train_test_split(
+  val_df,
+  test_size=0.33,
+  random_state=RANDOM_SEED
+)
+
+
+
+def create_dataset(df):
+
+  sequences = df.astype(np.float32).to_numpy().tolist()
+
+  dataset = [torch.tensor(s).unsqueeze(1).float() for s in sequences]
+
+  n_seq, seq_len, n_features = torch.stack(dataset).shape
+
+  return dataset, seq_len, n_features
+
+train_dataset, seq_len, n_features = create_dataset(train_df)
+val_dataset, _, _ = create_dataset(val_df)
+test_normal_dataset, _, _ = create_dataset(test_df)
+test_anomaly_dataset, _, _ = create_dataset(anomaly_df)
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, seq_len, n_features, embedding_dim):
+        super(Encoder, self).__init__()
+
+        self.seq_len, self.n_features = seq_len, n_features
+        self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
+        print(self.hidden_dim, self.embedding_dim)
+
+        self.rnn1 = nn.LSTM(
+            input_size=n_features,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=True
+        )
+
+        self.rnn2 = nn.LSTM(
+            input_size=self.hidden_dim,
+            hidden_size=embedding_dim,
+            num_layers=1,
+            batch_first=True
+        )
+
+    def forward(self, x):
+        x = x.reshape((1, self.seq_len, self.n_features))
+
+        x, (_, _) = self.rnn1(x)
+        x, (hidden_n, _) = self.rnn2(x)
+
+        return hidden_n.reshape((self.n_features, self.embedding_dim))
+
+class Decoder(nn.Module):
+
+  def __init__(self, seq_len, input_dim=9, n_features=1):
+    super(Decoder, self).__init__()
+
+    self.seq_len, self.input_dim = seq_len, input_dim
+    self.hidden_dim, self.n_features = 2 * input_dim, n_features
+
+    self.rnn1 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=input_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.rnn2 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=self.hidden_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.output_layer = nn.Linear(self.hidden_dim, n_features)
+
+  def forward(self, x):
+    x = x.repeat(self.seq_len, self.n_features)
+    x = x.reshape((self.n_features, self.seq_len, self.input_dim))
+
+    x, (hidden_n, cell_n) = self.rnn1(x)
+    x, (hidden_n, cell_n) = self.rnn2(x)
+    x = x.reshape((self.seq_len, self.hidden_dim))
+
+    return self.output_layer(x)
+
+
+class RecurrentAutoencoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=9):
+    super(RecurrentAutoencoder, self).__init__()
+
+    self.encoder = Encoder(seq_len, n_features, embedding_dim).to(device)
+    self.decoder = Decoder(seq_len, embedding_dim, n_features).to(device)
+
+  def forward(self, x):
+    x = self.encoder(x)
+    x = self.decoder(x)
+
+    return x
+
+
+model = RecurrentAutoencoder(seq_len, n_features, 128)
+model = model.to(device)
+
+
+def train_model(model, train_dataset, val_dataset, n_epochs):
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.L1Loss(reduction='sum').to(device)
+    history = dict(train=[], val=[])
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 10000.0
+
+    for epoch in range(1, n_epochs + 1):
+        model = model.train()
+
+        train_losses = []
+        for seq_true in train_dataset:
+            optimizer.zero_grad()
+
+            seq_true = seq_true.to(device)
+            seq_pred = model(seq_true)
+
+            loss = criterion(seq_pred, seq_true)
+
+            loss.backward()
+            optimizer.step()
+
+            train_losses.append(loss.item())
+
+        val_losses = []
+        model = model.eval()
+        with torch.no_grad():
+            for seq_true in val_dataset:
+                seq_true = seq_true.to(device)
+                seq_pred = model(seq_true)
+
+                loss = criterion(seq_pred, seq_true)
+                val_losses.append(loss.item())
+
+        train_loss = np.mean(train_losses)
+        val_loss = np.mean(val_losses)
+
+        history['train'].append(train_loss)
+        history['val'].append(val_loss)
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+        print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
+
+    model.load_state_dict(best_model_wts)
+    return model.eval(), history
+
+
+model, history = train_model(
+    model,
+    train_dataset,
+    val_dataset,
+    n_epochs=150
+)
+
+ax = plt.figure().gca()
+
+ax.plot(history['train'])
+ax.plot(history['val'])
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['train', 'test'])
+plt.title('Loss over training epochs')
+plt.show()
+
+
+MODEL_PATH = 'model.pth'
+torch.save(model, MODEL_PATH)
+
+model = torch.load('model.pth')
+model = model.to(device)
+
+
+def predict(model, dataset):
+    predictions, losses = [], []
+    criterion = nn.L1Loss(reduction='sum').to(device)
+    with torch.no_grad():
+        model = model.eval()
+        for seq_true in dataset:
+            seq_true = seq_true.to(device)
+            seq_pred = model(seq_true)
+
+            loss = criterion(seq_pred, seq_true)
+
+            predictions.append(seq_pred.cpu().numpy().flatten())
+            losses.append(loss.item())
+    return predictions, losses
+
+_, losses = predict(model, train_dataset)
+
+sns.displot(x=losses, kind="kde", bw_adjust=.25)
+plt.ylabel('Number of instances'
+           '(in hundreds)')
+
+plt.show()
+print(statistics.mean(losses))
+print(statistics.stdev(losses))
+THRESHOLD = 0.43
+
+predictions, pred_losses = predict(model, test_normal_dataset)
+sns.displot(pred_losses, kind="kde", bw_adjust=.25)
+plt.ylabel('Number of instances')
+plt.xlabel('MAE')
+plt.show()
+
+
+correct = sum(l <= THRESHOLD for l in pred_losses)
+print(f'Correct normal predictions: {correct}/{len(test_normal_dataset)}')
+
+anomaly_dataset = test_anomaly_dataset[:len(test_normal_dataset)]
+
+predictions, pred_losses = predict(model, anomaly_dataset)
+sns.displot(pred_losses, kind="kde", bw_adjust=.25)
+plt.ylabel('Number of instances')
+plt.xlabel('MAE')
+plt.show()
+
+correct = sum(l > THRESHOLD for l in pred_losses)
+print(f'Correct anomaly predictions: {correct}/{len(anomaly_dataset)}')
+
+def plot_prediction(data, model, title, ax):
+  predictions, pred_losses = predict(model, [data])
+
+  ax.plot(data, label='true')
+  ax.plot(predictions[0], label='reconstructed')
+  ax.set_title(f'{title} (loss: {np.around(pred_losses[0], 2)})')
+  ax.legend()
+
+fig, axs = plt.subplots(
+  nrows=2,
+  ncols=6,
+  sharey=True,
+  sharex=True,
+  figsize=(22, 8)
+)
+
+for i, data in enumerate(test_normal_dataset[:6]):
+  plot_prediction(data, model, title='Normal', ax=axs[0, i])
+
+for i, data in enumerate(test_anomaly_dataset[:6]):
+  plot_prediction(data, model, title='Anomaly', ax=axs[1, i])
+
+fig.tight_layout()
+plt.show()
+
